08cdf735f33e463497783d8c77efa166c00fc6b3
BaeTab <bhwoo48@gmail.com>
2024-03-17 12:55:41 +0900

    - First Commit My notepad

---
+++ myNotePad/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace myNotePad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(266, 438);
+            this.textBox1.TabIndex = 1;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(288, 488);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(294, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(230, 436);
+            this.listBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "저장";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 456);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "초기화";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(294, 456);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "저장";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(375, 456);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "초기화";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(456, 456);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "선택 삭제";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 488);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.splitter1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Notepad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ myNotePad/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace myNotePad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            initController();
+        }
+
+        private void initController()
+        {
+            button1.Click += Button1_Click;
+            button2.Click += Button2_Click;
+            button3.Click += Button3_Click;
+            button4.Click += Button4_Click;
+            button5.Click += Button5_Click;
+
+            textBox1.KeyDown += TextBox1_KeyDown;
+            listBox1.DoubleClick += Listbox1_DoubleClick;
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // 사용자가 Ctrl + S 키를 눌렀는지 확인합니다.
+            if (e.Control && e.KeyCode == Keys.S)
+            {
+                // textbox1의 내용을 listbox1에 추가합니다.
+                listBox1.Items.Add(textBox1.Text);
+
+                // 추가 후에는 textbox1을 비워줍니다.
+                textBox1.Clear();
+
+                // 입력을 처리했으므로 이벤트를 중단합니다.
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void Listbox1_DoubleClick(object sender, EventArgs e)
+        {
+            // 리스트 박스에서 선택된 항목을 가져와서 textbox1에 표시합니다.
+            if (listBox1.SelectedItem != null)
+            {
+                textBox1.Text = listBox1.SelectedItem.ToString();
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // textbox1의 내용을 listbox1에 추가합니다.
+            listBox1.Items.Add(textBox1.Text);
+
+            // 추가 후에는 textbox1을 비워줍니다.
+            textBox1.Clear();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength > 0)
+            {
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("초기화 할 내용이 없습니다.", "알림");
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            string logFolder = "log"; // 로그 폴더 이름
+            string fileName = Path.Combine(logFolder, DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt");
+            if (!Directory.Exists(logFolder))
+            {
+                Directory.CreateDirectory(logFolder);
+            }
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    foreach (var item in listBox1.Items)
+                    {
+                        writer.WriteLine(item.ToString());
+                    }
+                }
+                MessageBox.Show("파일이 log 폴더에 성공적으로 저장되었습니다.");
+                MessageBox.Show("리스트가 초기화 됩니다");
+                listBox1.Items.Clear();
+            }
+            catch (System.ObjectDisposedException)
+            {
+                MessageBox.Show("The System.IO.TextWriter is closed.");
+            }
+            catch (System.IO.IOException)
+            {
+                MessageBox.Show("An I/O error occurs.");
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                if (DialogResult.Yes == MessageBox.Show("정말 리스트를 삭제하시겠습니까?", "알림", MessageBoxButtons.YesNo))
+                {
+                    listBox1.Items.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("삭제할 리스트가 없습니다.", "알림");
+            }
+            return;
+        }
+
+        private void Button5_Click(object sender , EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("선택된 항목이 없습니다.");
+            }
+        }
+    }
+}
